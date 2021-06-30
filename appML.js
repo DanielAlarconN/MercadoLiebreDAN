55974144deb7be1a0ea493ba46d1fbd230d30efe
Daniel Alarcon <dnyan@MacBook-Air-de-Daniel.local>
2021-06-29 20:19:51 -0500

    septimo comit

--- appML.js
+++ appML.js
@@ -7,19 +7,19 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath))
 
-app.listen(3000, function() {
+app.listen(process.env.Port || 3000, function() {
     console.log('corriendo puerto 3030');
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 })
 
 app.get("/registro", function(req,res){
-    let htmlArchivo = path.resolve(__dirname, '/views/registro.html')
+    let htmlArchivo = path.resolve(__dirname, './views/registro.html')
     res.sendFile(htmlArchivo);
 })
 
 app.get("/log", function(req,res){
-    let htmlArchivo = path.resolve(__dirname, '/views/log.html')
+    let htmlArchivo = path.resolve(__dirname, './views/log.html')
     res.sendFile(htmlArchivo)})
